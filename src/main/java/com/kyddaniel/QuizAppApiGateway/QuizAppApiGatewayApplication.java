b77a94631814e1927ec9e778c30561337b846043
kyddaniel <mksuenaa@connect.ust.hk>
2024-12-15 20:48:31 +0800

    Initial commit

---
+++ src/main/java/com/kyddaniel/QuizAppApiGateway/QuizAppApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kyddaniel.QuizAppApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppApiGatewayApplication.class, args);
+	}
+
+}
